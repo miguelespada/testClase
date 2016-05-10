9471524d4dda45fa74e7082faada2b6e281fe7b7
miguelespada <miguel.valero.espada@gmail.com>
2016-05-10 14:34:29 +0200

    add mongo production

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,5 @@
 puts "*" * 40
 puts "Connecting to redis... "
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+$redis = Redis.new(:host => 'p659i2daf1a02nctnuv09mh36dr@ec2-54-217-222-237.eu-west-1.compute.amazonaws.com', :port => 9159)
+
 puts "*" * 40
